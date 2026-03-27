b27ac541b493ccf4199d05deb4818a778d93f484
xiamoqi <3319640549@qq.com>
2026-03-27 23:08:42 +0800

    初始化 YifaSilverGuard 项目

---
+++ src/main/java/com/yifasilverguard/YifaSilverGuardApplication.java
@@ -0,0 +1,16 @@
+package com.yifasilverguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiamoqi
+ */
+@SpringBootApplication
+public class YifaSilverGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YifaSilverGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yifasilverguard/YifaSilverGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifasilverguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YifaSilverGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
